77558b2c3ff7cc0c8234dab6e6e366f1556ddabe
Jephthah Mensah <99607903+wesucceed@users.noreply.github.com>
2023-09-15 12:59:40 -0400

    Update settings.py-removed secrete

--- ftf/ftf/settings.py
+++ ftf/ftf/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-969$pwm(*_fk+yii-(242nmyx)j%ge1lh$)dux+@(sw$9un9kx'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
