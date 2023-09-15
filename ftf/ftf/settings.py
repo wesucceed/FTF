476c6e562aca8dc5c2bff9d7b9d28b42b3e981c0
Jephthah Mensah <jephthahmensah@dhcp-vl2041-46812.eduroam.cornell.edu>
2023-09-15 13:29:40 -0400

    removed key

--- ftf/ftf/settings.py
+++ ftf/ftf/settings.py
@@ -21,11 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-<<<<<<< HEAD
-SECRET_KEY = os.environ["SECRET_KEY"]
-=======
 SECRET_KEY = ''
->>>>>>> 77558b2c3ff7cc0c8234dab6e6e366f1556ddabe
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
